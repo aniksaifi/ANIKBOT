3156844142cf9f2367620181691bee852eb195f2
aniksaifi <laradiebitch@gmail.com>
2026-07-25 20:25:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
     startimage: 'https://files.manuscdn.com/user_upload_by_module/session_file/310519663853348353/tPjwxbigrqUrvfqQ.png',
-    ownerNumber: process.env.OWNER_NUMBER || '919355862006',
+    ownerNumber: process.env.OWNER_NUMBER || '923314836942',
     botName: 'ANIK BOT',
     ownerName: 'ANIK',
     tgOwnerId: process.env.OWNER_TELEGRAM_ID || '8695364795',
