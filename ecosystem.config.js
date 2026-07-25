0fc420db1fea1754a045afc0feedf33637bc2b0d
aniksaifi <aniksaifi@example.com>
2026-07-25 12:13:25 +0000

    Initial commit

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'ANIK-BOT',
+    script: './index.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    },
+    log_file: './logs/combined.log',
+    out_file: './logs/out.log',
+    error_file: './logs/error.log',
+    time: true
+  }]
+};
---
+++ settings.js
@@ -0,0 +1,11 @@
+module.exports = {
+    startimage: 'https://files.catbox.moe/anls16.jpg',
+    ownerNumber: process.env.OWNER_NUMBER || '919355862006',
+    botName: 'ANIK BOT',
+    ownerName: 'ANIK',
+    tgOwnerId: process.env.OWNER_TELEGRAM_ID || '8695364795',
+    premiumUsers: [],
+    connectedBots: [],
+    version: '3.0.0',
+    prefix: '.'
+};
